6ce3ee0f35acdbbdddad6d0f02e1451662b4223e
Keshavvats9470 <keshavvats9470@gmail.com>
2025-01-24 21:17:54 +0530

    Updated

--- frontend/src/components/Post/PostedJobsCard.js
+++ frontend/src/components/Post/PostedJobsCard.js
@@ -682,6 +682,7 @@ const handleRemoveFromWishlist = async (jobId) => {
                           </svg>
                         </button>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
                         {wishlistJobs.includes(job._id) ? (
     <FaBookmark
       onClick={() => handleRemoveFromWishlist(job._id)}
@@ -750,6 +751,12 @@ const handleRemoveFromWishlist = async (jobId) => {
                         </div>
                       </dl>
 =======
+=======
+                      </div>
+
+                      <hr className='mt-2' />
+
+>>>>>>> Stashed changes
                       <div className='flex justify-between mt-1 px-2 py-3'>
                         <button className='py-1 px-2 text-xs rounded-full bg-gray-200'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.0" stroke="currentColor" class="size-4">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
@@ -760,6 +767,9 @@ const handleRemoveFromWishlist = async (jobId) => {
                         </svg>
                         </button>
                       </div>
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
                     </motion.li>
                   ))
